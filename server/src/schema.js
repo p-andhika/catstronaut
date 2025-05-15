505fab4e0d29253be08541b16512366c567c9c91
Andhika Prakasiwi <andhika.prakasiwi@agriaku.com>
2025-05-15 14:56:27 +0700

    feat(schema): migrate to TypeScript for better type safety

--- server/src/schema.js
+++
@@ -1,5 +0,0 @@
-const gql = require("graphql-tag");
-
-const typeDefs = gql``;
-
-module.export = typeDefs;
---
+++ server/src/schema.ts
@@ -0,0 +1,28 @@
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+  # Scema definitions go here...
+
+  "A track is a group of Modules that teaches about a spesific topic"
+  type Track {
+    # Fields go here
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+  }
+
+  "Author of a complete Track or a Module"
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
+
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+  }
+`;
